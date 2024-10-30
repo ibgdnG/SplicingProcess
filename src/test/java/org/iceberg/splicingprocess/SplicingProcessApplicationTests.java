1c58e63cdeaf8bb132214c0fddf6241ba496a467
ice <ibgdn4@gmail.com>
2024-10-30 17:51:38 +0800

    init.

---
+++ src/test/java/org/iceberg/splicingprocess/SplicingProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iceberg.splicingprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplicingProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
